94389f6cf399d560e0d507d3a016d6ae6abea361
Alexei <alexei@entropy.energy>
2020-07-27 22:52:13 +1000

    Fixed config search paths

--- nexus/config.py
+++ nexus/config.py
@@ -98,9 +98,13 @@ def get_config():
 
     config = default_config.copy()
 
+    # any of these config files should define a config dictionary with keys to update
+    # the default config
+
+    # TODO make robust against errors in config files
     possible_configs = [
-        Path("~/Library/Application Support/Ook/config.py").expanduser(),
-        Path("~/.config/ook/config.py").expanduser(),
+        Path("~/Library/Application Support/Nexus/config.py").expanduser(),
+        Path("~/.config/nexus/config.py").expanduser(),
     ]
 
     for c in possible_configs:
